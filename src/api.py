6ac394955cf144ea4f354362bc06b9960217d460
PhilippMarquardt <p.marquardt15@googlemail.com>
2023-07-25 14:08:55 +0200

    Allow dict to be passed to trainer

--- src/api.py
+++ src/api.py
@@ -4,9 +4,12 @@ import yaml
 
 
 def get_runner(config_file):
-    # open configs file yaml
-    with open(config_file) as f:
-        settings = yaml.load(f, Loader=yaml.FullLoader)
+    if isinstance(config_file, dict):
+        settings = config_file
+    else:
+        with open(config_file) as f:
+            settings = yaml.load(f, Loader=yaml.FullLoader)
+
     cfg = ConfigModifierRegistry.config_modifiers[settings["model_name"]](settings)
     runner = Runner.from_cfg(cfg)
     return runner
