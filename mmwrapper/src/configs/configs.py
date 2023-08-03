e41686f130306ec72791ba9101deedc9d729029d
PhilippMarquardt <p.marquardt15@googlemail.com>
2023-08-03 16:10:34 +0200

    Fix object detection

--- mmwrapper/src/configs/configs.py
+++ mmwrapper/src/configs/configs.py
@@ -266,16 +266,69 @@ def modfiy_mask2former_r50_config(settings, cfg_path_end="mask2former_r50.py"):
 def modify_dino_config(settings, cfg_path_end="dino_r50.py"):
 
     train_pipeline = [
-        dict(type="LoadImageFromFile", to_float32=True, backend_args=None),
-        dict(type="LoadAnnotations", with_bbox=True, with_mask=True),
+        dict(type="LoadImageFromFile", backend_args=None),
+        dict(type="LoadAnnotations", with_bbox=True),
         dict(type="RandomFlip", prob=0.5),
-        dict(type="Resize", scale=settings["image_size"], keep_ratio=False),
+        dict(
+            type="RandomChoice",
+            transforms=[
+                [
+                    {
+                        "type": "RandomChoiceResize",
+                        "scales": [
+                            (480, 1333),
+                            (512, 1333),
+                            (544, 1333),
+                            (576, 1333),
+                            (608, 1333),
+                            (640, 1333),
+                            (672, 1333),
+                            (704, 1333),
+                            (736, 1333),
+                            (768, 1333),
+                            (800, 1333),
+                        ],
+                        "keep_ratio": True,
+                    }
+                ],
+                [
+                    {
+                        "type": "RandomChoiceResize",
+                        "scales": [(400, 4200), (500, 4200), (600, 4200)],
+                        "keep_ratio": True,
+                    },
+                    {
+                        "type": "RandomCrop",
+                        "crop_type": "absolute_range",
+                        "crop_size": (384, 600),
+                        "allow_negative_crop": True,
+                    },
+                    {
+                        "type": "RandomChoiceResize",
+                        "scales": [
+                            (480, 1333),
+                            (512, 1333),
+                            (544, 1333),
+                            (576, 1333),
+                            (608, 1333),
+                            (640, 1333),
+                            (672, 1333),
+                            (704, 1333),
+                            (736, 1333),
+                            (768, 1333),
+                            (800, 1333),
+                        ],
+                        "keep_ratio": True,
+                    },
+                ],
+            ],
+        ),
         dict(type="PackDetInputs"),
     ]
     test_pipeline = [
-        dict(type="LoadImageFromFile", to_float32=True, backend_args=None),
-        dict(type="Resize", scale=settings["image_size"], keep_ratio=False),
-        dict(type="LoadAnnotations", with_bbox=True, with_mask=True),
+        dict(type="LoadImageFromFile", backend_args=None),
+        dict(type="Resize", scale=(1333, 800), keep_ratio=True),
+        dict(type="LoadAnnotations", with_bbox=True),
         dict(
             type="PackDetInputs",
             meta_keys=("img_id", "img_path", "ori_shape", "img_shape", "scale_factor"),
