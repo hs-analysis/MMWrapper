d63871bcefac27f4b1ab2ee57f92b7ac96129a1a
PhilippMarquardt <p.marquardt15@googlemail.com>
2023-09-04 00:26:42 +0200

    Add inference model

--- mmwrapper/src/api.py
+++ mmwrapper/src/api.py
@@ -1,6 +1,12 @@
 from mmengine.runner import Runner
-from .configs.configs import ConfigModifierRegistry
+from mmengine import Config
+from mmseg.apis import init_model, inference_model
+from mmdet.apis import init_detector, inference_detector
+
+
 import yaml
+
+from .configs.configs import ConfigModifierRegistry
 
 
 def get_runner(config_file):
@@ -15,5 +21,20 @@ def get_runner(config_file):
     return runner
 
 
-def get_inference_model():
-    pass
+class InferenceModel:
+    def __init__(self, checkpoint_file, config_file, device="cuda"):
+        self.is_detection = (
+            Config.fromfile(config_file)["train_pipeline"][-1]["type"]
+            == "PackDetInputs"
+        )
+        print(Config.fromfile(config_file)["train_pipeline"][-1])
+        print(self.is_detection)
+        if self.is_detection:
+            self.model = init_detector(config_file, checkpoint_file, device=device)
+            self.forward = inference_detector
+        else:
+            self.model = init_model(config_file, checkpoint_file, device=device)
+            self.forward = inference_model
+
+    def predict(self, imgs):
+        return self.forward(self.model, imgs)
